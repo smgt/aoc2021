b1ab0208baef8dee0dce1028bbbfa14be5bc735e
Simon Gate <simon@kampgate.se>
2021-12-02 10:09:30 +0100

    ...

--- src/01.cr
+++ src/01.cr
@@ -1,36 +1,36 @@
-#!/usr/bin/env crystal
 # First step
-inc = 0
-prev = nil
+
+def work(input)
+  inc = 0
+  prev = nil
+
+  input.each do |measure|
+    if prev.nil?
+      prev = measure
+      next
+    end
+
+    if measure > prev
+      inc += 1
+    end
+
+    prev = measure
+  end
+
+  return inc
+end
+
 input = File.read_lines("01.input").map do |x|
   x.chomp.to_i
 end
-input.each do |measure|
-  if prev.nil?
-    prev = measure
-    next
-  end
-  if measure > prev
-    inc += 1
-  end
-  prev = measure
+
+a = work(input)
+puts "1A: #{a}"
+
+input2 = [] of Int32
+input.each_cons(3) do |c|
+  input2 << c.sum
 end
+b = work(input2)
 
-puts "1A: #{inc}"
-
-inc2 = 0
-prev = nil
-input.each_cons(3) do |cons|
-  sum = cons.sum
-  if prev.nil?
-    prev = sum
-    next
-  end
-
-  if sum > prev
-    inc2 += 1
-  end
-  prev = sum
-end
-
-puts "1B: #{inc2}"
+puts "1B: #{b}"
